fec4cc8034bc274e4ec21e16e48486db059a4d70
Rednir <davran.dilshat@gmail.com>
2021-05-11 18:30:59 +0100

    rename variable name

--- src/data.py
+++ src/data.py
@@ -4,7 +4,7 @@ import logging
 
 
 GUILD_DATA_FILE_PATH = "guild_data.json"
-guild_data_dictionary = None
+__guild_data_dict = None
 
 
 def get_guild_data(guild_id) -> dict:
@@ -12,12 +12,12 @@ def get_guild_data(guild_id) -> dict:
     Returns a copy of a guild's data and creates new if necessary.
     """
     guild_id = str(guild_id)
-    if guild_id not in guild_data_dictionary:
+    if guild_id not in __guild_data_dict:
         # Initialize data for this guild
-        guild_data_dictionary[guild_id] = {
+        __guild_data_dict[guild_id] = {
             "keywords": {}
         }
-    return guild_data_dictionary[guild_id].copy()
+    return __guild_data_dict[guild_id].copy()
 
 
 def set_guild_data(guild_id, new_data: dict, also_write_to_file: bool = True):
@@ -25,29 +25,29 @@ def set_guild_data(guild_id, new_data: dict, also_write_to_file: bool = True):
     Overwrites a guild's data with new data.
     """
     guild_id = str(guild_id)
-    guild_data_dictionary[guild_id] = new_data
+    __guild_data_dict[guild_id] = new_data
     if also_write_to_file:
         write_to_file()
 
 
 def write_to_file():
-    keywords_json = json.dumps(guild_data_dictionary)
+    keywords_json = json.dumps(__guild_data_dict)
     with open(GUILD_DATA_FILE_PATH, "w") as guild_data_file:
         guild_data_file.write(keywords_json)
     logging.log(logging.INFO, f"Data was written to {GUILD_DATA_FILE_PATH}")
 
 
 def load_from_file():
-    global guild_data_dictionary
+    global __guild_data_dict
 
     if os.path.exists(GUILD_DATA_FILE_PATH):
         with open(GUILD_DATA_FILE_PATH, "r") as data_file:
-            guild_data_dictionary = json.loads(data_file.read())
+            __guild_data_dict = json.loads(data_file.read())
     else:
         with open(GUILD_DATA_FILE_PATH, "w") as data_file:
             data_file.write("{}")
-        guild_data_dictionary = {}
+        __guild_data_dict = {}
 
 
-if guild_data_dictionary == None:
+if __guild_data_dict == None:
     load_from_file()
