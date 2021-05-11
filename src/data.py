75812030a350668fca72fd2740c1b1b40571dadb
Rednir <davran.dilshat@gmail.com>
2021-05-11 17:03:43 +0100

    probably best to not use classes

--- src/data.py
+++ src/data.py
@@ -1,31 +1,33 @@
 import json
 
 
-class Data:
-    guild_data_dictionary = {}
+guild_data_dictionary = {}
 
-    def __init__(self):
-        self.load()
 
-    def save(self):
-        keywords_json = json.dumps(self.guild_data_dictionary)
-        with open("guild_data.json", "w") as guild_data_file:
-            guild_data_file.write(keywords_json)
+def save():
+    keywords_json = json.dumps(guild_data_dictionary)
+    with open("guild_data.json", "w") as guild_data_file:
+        guild_data_file.write(keywords_json)
 
-    def load(self):
-        with open("guild_data.json", "r") as guild_data_file:
-            self.guild_data_dictionary = json.loads(guild_data_file.read())
-    
-    def get_guild_data(self, guild_id):
-        guild_id = str(guild_id)
-        if guild_id not in self.guild_data_dictionary:
-            # Initialize data for this guild
-            self.guild_data_dictionary[guild_id] = {
-                "keywords": {}
-            }
-        return self.guild_data_dictionary[guild_id]
 
-    def set_guild_data(self, guild_id, new_data: dict, also_save: bool = False):
-        guild_id = str(guild_id)
-        self.guild_data_dictionary[guild_id] = new_data
-        if also_save: self.save()
+def load():
+    global guild_data_dictionary
+    with open("guild_data.json", "r") as guild_data_file:
+        guild_data_dictionary = json.loads(guild_data_file.read())
+
+
+def get_guild_data(guild_id):
+    guild_id = str(guild_id)
+    if guild_id not in guild_data_dictionary:
+        # Initialize data for this guild
+        guild_data_dictionary[guild_id] = {
+            "keywords": {}
+        }
+    return guild_data_dictionary[guild_id]
+
+
+def set_guild_data(guild_id, new_data: dict, also_save: bool = False):
+    guild_id = str(guild_id)
+    guild_data_dictionary[guild_id] = new_data
+    if also_save:
+        save()
