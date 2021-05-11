01b1d02030db9320f2a5f808696f4bed4fb94509
Rednir <davran.dilshat@gmail.com>
2021-05-11 17:35:09 +0100

    add docstrings

--- src/data.py
+++ src/data.py
@@ -1,22 +1,14 @@
 import json
+from typing import Dict
 
 
 guild_data_dictionary = {}
 
 
-def save():
-    keywords_json = json.dumps(guild_data_dictionary)
-    with open("guild_data.json", "w") as guild_data_file:
-        guild_data_file.write(keywords_json)
-
-
-def load():
-    global guild_data_dictionary
-    with open("guild_data.json", "r") as guild_data_file:
-        guild_data_dictionary = json.loads(guild_data_file.read())
-
-
-def get_guild_data(guild_id):
+def get_guild_data(guild_id) -> dict:
+    """
+    Returns a copy of a guild's data and creates new if necessary.
+    """
     guild_id = str(guild_id)
     if guild_id not in guild_data_dictionary:
         # Initialize data for this guild
@@ -26,8 +18,23 @@ def get_guild_data(guild_id):
     return guild_data_dictionary[guild_id].copy()
 
 
-def set_guild_data(guild_id, new_data: dict, also_save: bool = False):
+def set_guild_data(guild_id, new_data: dict, also_write_to_file: bool = True):
+    """
+    Overwrites a guild's data with new data.
+    """
     guild_id = str(guild_id)
     guild_data_dictionary[guild_id] = new_data
-    if also_save:
-        save()
+    if also_write_to_file:
+        write_to_file()
+
+
+def write_to_file():
+    keywords_json = json.dumps(guild_data_dictionary)
+    with open("guild_data.json", "w") as guild_data_file:
+        guild_data_file.write(keywords_json)
+
+
+def load_from_file():
+    global guild_data_dictionary
+    with open("guild_data.json", "r") as guild_data_file:
+        guild_data_dictionary = json.loads(guild_data_file.read())
