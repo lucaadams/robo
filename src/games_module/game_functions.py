56430125080770cdc7f1f39292af1c85be28c14c
Luca <lucaads2006@gmail.com>
2021-05-15 10:58:13 +0100

    dont run code if not necessary

--- src/games_module/game_functions.py
+++ src/games_module/game_functions.py
@@ -5,11 +5,20 @@ import games_module.counting
 async def start_game(guild_id, message):
     try:
         game = message.content.split(" ")[2]
-    except:
+    except IndexError:
         await message.channel.send(embed=verbose.embeds.embed_error_message("Must specify a game."))
         return
 
+    if game == "counting":
+        increment = await set_increment(message)
+        await games_module.counting.start_counting(guild_id, message, increment)
+    else:
+        await message.channel.send(embed=verbose.embeds.embed_error_message("That game does not exist."))
+
+
+async def set_increment(message):
     increment = 1
+
     try:
         increment = float(message.content.split(" ")[3])
     except ValueError:
@@ -17,7 +26,4 @@ async def start_game(guild_id, message):
     except IndexError:
         pass
 
-    if game == "counting":
-        await games_module.counting.start_counting(guild_id, message, increment)
-    else:
-        await message.channel.send(embed=verbose.embeds.embed_error_message("That game does not exist."))
+    return increment
