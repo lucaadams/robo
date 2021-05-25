b6c3d726e601c47ab5c2344a367064ae0c919a8a
Luca <lucaads2006@gmail.com>
2021-05-25 18:18:00 +0100

    disable reactions on old messages to reduce lag and renamed sent_stats_messages to sent_stats_messages_with_reactions for clarity

--- src/modules/minecraft/hypixel.py
+++ src/modules/minecraft/hypixel.py
@@ -1,9 +1,7 @@
-from discord import user
 import requests
 import logging
-import discord
+import time
 
-import bot
 import data
 import verbose.embeds
 import cache
@@ -20,7 +18,7 @@ if hypixel_api_key == "REPLACE THIS TEXT WITH YOUR HYPIXEL API KEY":
         "Your hypixel API key had not been set. Bedwars and SkyWars stats commands will not work. Get one by going in-game to the hypixel server and typing `/api new`.")
 
 recent_searches = cache.Cache()
-sent_stats_messages = []
+sent_stats_messages_with_reactions = []
 
 emoji_list = ["0️⃣", "1️⃣", "2️⃣", "3️⃣", "4️⃣"]
 stop_emoji = "⏹️"
@@ -31,6 +29,7 @@ class StatsMessage:
         self.message = message
         self.base_player_data = base_player_data
         self.gamemode = gamemode
+        self.time_sent = time.time()
 
 
 async def hypixel_command_handler(message):
@@ -90,8 +89,9 @@ async def hypixel_command_handler(message):
             bw_stats_message = await message.channel.send(embed=modules.minecraft.stats.bedwars_stats.embed_bedwars_stats(base_player_data, 0))
             sent_message = StatsMessage(
                 bw_stats_message, base_player_data, "bw")
-            sent_stats_messages.append(sent_message)
+            sent_stats_messages_with_reactions.append(sent_message)
             await add_reaction(bw_stats_message)
+            await disable_old_reactions()
         except TypeError:
             await message.channel.send(embed=verbose.embeds.embed_sorry_message("I could not find a user with that name. Please check spelling and try again."))
             return
@@ -112,7 +112,7 @@ async def hypixel_command_handler(message):
             sw_stats_message = await message.channel.send(embed=modules.minecraft.stats.skywars_stats.embed_skywars_stats(base_player_data, 0))
             sent_message = StatsMessage(
                 sw_stats_message, base_player_data, "sw")
-            sent_stats_messages.append(sent_message)
+            sent_stats_messages_with_reactions.append(sent_message)
             await add_reaction(sw_stats_message)
         except TypeError:
             await message.channel.send(embed=verbose.embeds.embed_sorry_message("I could not find a user with that name. Please check spelling and try again."))
@@ -131,7 +131,7 @@ async def add_reaction(bot_message):
 async def change_page(bot_message, reaction):
     # first, get the stats of the user that the sent message was about
     message_index_in_sent_messages = None
-    for index, stats_message in enumerate(sent_stats_messages):
+    for index, stats_message in enumerate(sent_stats_messages_with_reactions):
         if bot_message == stats_message.message:
             message_index_in_sent_messages = index
 
@@ -140,8 +140,8 @@ async def change_page(bot_message, reaction):
 
     if reaction.emoji == stop_emoji:
         # remove all reactions
-        await sent_stats_messages[message_index_in_sent_messages].message.clear_reactions()
-        sent_stats_messages.pop(message_index_in_sent_messages)
+        await sent_stats_messages_with_reactions[message_index_in_sent_messages].message.clear_reactions()
+        sent_stats_messages_with_reactions.pop(message_index_in_sent_messages)
         return
 
     # get required page number
@@ -154,13 +154,29 @@ async def change_page(bot_message, reaction):
     if page_number == -1:
         return
 
-    if sent_stats_messages[message_index_in_sent_messages].gamemode == "bw":
+    if sent_stats_messages_with_reactions[message_index_in_sent_messages].gamemode == "bw":
         await bot_message.edit(embed=modules.minecraft.stats.bedwars_stats.embed_bedwars_stats(
-            sent_stats_messages[message_index_in_sent_messages].base_player_data, page_number))
+            sent_stats_messages_with_reactions[message_index_in_sent_messages].base_player_data, page_number))
 
-    elif sent_stats_messages[message_index_in_sent_messages].gamemode == "sw":
+    elif sent_stats_messages_with_reactions[message_index_in_sent_messages].gamemode == "sw":
         await bot_message.edit(embed=modules.minecraft.stats.skywars_stats.embed_skywars_stats(
-            sent_stats_messages[message_index_in_sent_messages].base_player_data, page_number))
+            sent_stats_messages_with_reactions[message_index_in_sent_messages].base_player_data, page_number))
+
+
+async def disable_old_reactions():
+    """
+    disable reactions on old messages to reduce lag
+    """
+    # for each sent stats message with reactions, if it is older than 10 mins old, clear reactions
+    for index, stats_message in enumerate(sent_stats_messages_with_reactions):
+        if time.time() - stats_message.time_sent > 600:
+            await stats_message.message.clear_reactions()
+            sent_stats_messages_with_reactions.pop(index)
+
+    # there cant be more than 10 stats messages with reactions enabled at once
+    while len(sent_stats_messages_with_reactions) > 10:
+        await sent_stats_messages_with_reactions[0].message.clear_reactions()
+        sent_stats_messages_with_reactions.pop(0)
 
 
 def sw_xp_to_level(xp):
