96380f47714b46247e6f84a83ce9bd79609b5650
Luca <lucaads2006@gmail.com>
2021-06-05 11:50:36 +0100

    fix bug with level showing up wrong on discord

--- src/modules/minecraft/hypixel.py
+++ src/modules/minecraft/hypixel.py
@@ -102,8 +102,9 @@ async def hypixel_command_handler(message):
     elif game == "sw" or game == "skywars":
         try:
             skywars_data = hypixel_data["player"]["stats"]["SkyWars"]
-            level = skywars_data['levelFormatted'][-3:].strip('§f')
-            level = level[:-1] + "\\" + level[-1]
+            level = skywars_data['levelFormatted']
+            # kinda complicated but all it does is make sure it shows up correctly in discord
+            level = (level[-3:].strip('§f'))[:-1] + "\\" + (level[-3:].strip('§f'))[-1] if level.__contains__("f") else level[-2:]
             username = f"[{level}] {hypixel_data['player']['displayname']}"
             base_player_data = {
                 "username": username, "gamemode": "sw", "gamemode_specific_data": skywars_data,
