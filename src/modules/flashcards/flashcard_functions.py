69629c71920dd4777aa6f23cb7563b48ab5db787
Luca <lucaads2006@gmail.com>
2021-06-05 18:44:32 +0100

    cleaned up flashcard_functions.py

--- src/modules/flashcards/flashcard_functions.py
+++ src/modules/flashcards/flashcard_functions.py
@@ -29,8 +29,6 @@ async def flashcard_command_handler(message):
     guild_id = str(message.guild.id)
     second_parameter = message.content.split()[2]
 
-    # !robo flashcards use name
-
     if guild_id not in flashcard_data:
         flashcard_data[guild_id] = DEFAULT_GUILD_FLASHCARD_DATA.copy()
 
@@ -45,59 +43,16 @@ async def flashcard_command_handler(message):
         await create_new_flashcards_set(message)
 
     elif second_parameter == "end-creation" or second_parameter == "stop-creation":
-        if not flashcard_data[guild_id]["flashcard_creation_in_progress"]:
-            await message.channel.send(embed=verbose.embeds.embed_error_message("You cannot end creation if no creation has started."))
-
-        if message.author != flashcard_data[guild_id]["user"]:
-            await message.channel.send(embed=verbose.embeds.embed_sorry_message("Only the flashcard set creator can end creation."))
-            return
-
-        flashcard_set_name = flashcard_data[guild_id]["current_flashcard_set_name"]
-        guild_data = data.get_guild_data(guild_id)
-        guild_data["flashcard_sets"][flashcard_set_name] = flashcard_data[guild_id]["current_flashcard_set"].copy()
-        data.set_guild_data(guild_id, guild_data)
-
-        await message.channel.send(embed=verbose.embeds.embed_successful_action(f"Your flashcard set `{flashcard_set_name}` has been saved."))
-        # reset guild flashcard data 
-        flashcard_data[guild_id] = DEFAULT_GUILD_FLASHCARD_DATA.copy()
+        await end_flashcard_creation(message)
 
     elif second_parameter == "use":
-        try:
-            flashcard_set_to_use = message.content.split()[3]
-        except IndexError:
-            await message.channel.send(embed=verbose.embeds.embed_error_message("You must specify a flashcard set to use."))
-            return
+        await setup_flashcard_use(message)
 
-        flashcard_sets = data.get_guild_data(guild_id)["flashcard_sets"]
-
-        if flashcard_set_to_use not in flashcard_sets.keys():
-            await message.channel.send(embed=verbose.embeds.embed_sorry_message("That flashcard set does not exist. Please check spelling and try again."))
-            return
-
-        if flashcard_data[guild_id]["flashcard_creation_in_progress"]:
-            await active_flashcard_sets[guild_id]["message"].clear_reactions()
-
-        active_flashcard_sets[guild_id] = {
-            "name_of_set": flashcard_set_to_use,
-            "active_set": flashcard_sets[flashcard_set_to_use],
-            "message": await message.channel.send(embed=flashcard_embed(flashcard_set_to_use, flashcard_sets[flashcard_set_to_use], 0, "front")),
-            "index": 0,
-            "current_side": "front"
-        }
-
-        for emoji in emoji_list:
-            await active_flashcard_sets[guild_id]["message"].add_reaction(emoji)
+    elif second_parameter == "edit":
+        await edit_flashcard_set(message)
 
     elif second_parameter == "list":
-        flashcard_sets = data.get_guild_data(guild_id)["flashcard_sets"]
-        total_desc = ""
-        for flashcard_set in flashcard_sets:
-            total_desc += f"`{flashcard_set}` - {len(flashcard_sets[flashcard_set])} terms\n"
-        if total_desc == "":
-            await message.channel.send(embed=verbose.embeds.embed_response_without_title("You do not have any saved flashcard sets."))
-            return
-        
-        await message.channel.send(embed=verbose.embeds.embed_response("Saved flashcard sets:", total_desc))
+        await send_flashcard_set_list(message)
 
     else:
         await message.channel.send(embed=verbose.embeds.embed_error_message("That command does not exist."))
@@ -135,6 +90,57 @@ async def add_flashcard_to_set(message):
     flashcard_data[guild_id]["current_flashcard_set"][flashcard_front] = flashcard_definition
 
 
+async def end_flashcard_creation(message):
+    guild_id = str(message.guild.id)
+
+    if not flashcard_data[guild_id]["flashcard_creation_in_progress"]:
+        await message.channel.send(embed=verbose.embeds.embed_error_message("You cannot end creation if no creation has started."))
+
+    if message.author != flashcard_data[guild_id]["user"]:
+        await message.channel.send(embed=verbose.embeds.embed_sorry_message("Only the flashcard set creator can end creation."))
+        return
+
+    flashcard_set_name = flashcard_data[guild_id]["current_flashcard_set_name"]
+    guild_data = data.get_guild_data(guild_id)
+    guild_data["flashcard_sets"][flashcard_set_name] = flashcard_data[guild_id]["current_flashcard_set"].copy()
+    data.set_guild_data(guild_id, guild_data)
+
+    await message.channel.send(embed=verbose.embeds.embed_successful_action(f"Your flashcard set `{flashcard_set_name}` has been saved."))
+
+    # reset guild flashcard data 
+    flashcard_data[guild_id] = DEFAULT_GUILD_FLASHCARD_DATA.copy()
+
+
+async def setup_flashcard_use(message):
+    guild_id = str(message.guild.id)
+
+    try:
+        flashcard_set_to_use = message.content.split()[3]
+    except IndexError:
+        await message.channel.send(embed=verbose.embeds.embed_error_message("You must specify a flashcard set to use."))
+        return
+
+    flashcard_sets = data.get_guild_data(guild_id)["flashcard_sets"]
+
+    if flashcard_set_to_use not in flashcard_sets.keys():
+        await message.channel.send(embed=verbose.embeds.embed_sorry_message("That flashcard set does not exist. Please check spelling and try again."))
+        return
+
+    if flashcard_data[guild_id]["flashcard_creation_in_progress"]:
+        await active_flashcard_sets[guild_id]["message"].clear_reactions()
+
+    active_flashcard_sets[guild_id] = {
+        "name_of_set": flashcard_set_to_use,
+        "active_set": flashcard_sets[flashcard_set_to_use],
+        "message": await message.channel.send(embed=flashcard_embed(flashcard_set_to_use, flashcard_sets[flashcard_set_to_use], 0, "front")),
+        "index": 0,
+        "current_side": "front"
+    }
+
+    for emoji in emoji_list:
+        await active_flashcard_sets[guild_id]["message"].add_reaction(emoji)
+
+
 async def use_flashcards(bot_message, reaction):
     guild_id = str(bot_message.guild.id)
     if guild_id in active_flashcard_sets and bot_message.id != active_flashcard_sets[guild_id]["message"].id:
@@ -168,7 +174,19 @@ async def use_flashcards(bot_message, reaction):
         flashcard_set_for_guild["current_side"]
     ))
 
-    # await bot_message.edit(file=discord.File(modules.flashcards.flashcard_creator.write_to_image("kasmnjhf")))
+
+async def send_flashcard_set_list(message):
+    guild_id = str(message.guild.id)
+
+    flashcard_sets = data.get_guild_data(guild_id)["flashcard_sets"]
+    total_desc = ""
+    for flashcard_set in flashcard_sets:
+        total_desc += f"`{flashcard_set}` - {len(flashcard_sets[flashcard_set])} terms\n"
+    if total_desc == "":
+        await message.channel.send(embed=verbose.embeds.embed_response_without_title("You do not have any saved flashcard sets."))
+        return
+    
+    await message.channel.send(embed=verbose.embeds.embed_response("Saved flashcard sets:", total_desc))
 
 
 def flashcard_embed(name_of_set, flashcard_set, index, side):
